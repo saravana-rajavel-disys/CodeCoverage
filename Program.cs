dcef09380cd5b2e75471cf03d7fae4cbb1b3cd7c
Saravanakumar Rajavel <saravana.rajavel@disys.com>
2021-04-08 18:15:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodeCoverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            if (x > 3 && y < 6 || z == 2)
+            {
+                ExecuteFunctionA();
+            }
+            else
+            {
+                ExecuteFunctionB();
+            }
+        }
+
+        private static void ExecuteFunctionA()
+        {
+
+        }
+
+        private static void ExecuteFunctionB()
+        {
+
+        }
+    }
+}
